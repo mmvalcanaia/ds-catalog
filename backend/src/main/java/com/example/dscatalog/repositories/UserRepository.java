f275e00498f35370262fbced62dccfa9fa1705d7
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2021-10-10 15:17:23 +0200

    Custom ConstraintValidator

--- backend/src/main/java/com/example/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/example/dscatalog/repositories/UserRepository.java
@@ -7,5 +7,8 @@ import com.example.dscatalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+	User findByEmail(String email);
 
 }
---
+++ backend/src/main/java/com/example/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,43 @@
+package com.example.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.dscatalog.dto.UserInsertDTO;
+import com.example.dscatalog.entities.User;
+import com.example.dscatalog.repositories.UserRepository;
+import com.example.dscatalog.resources.exceptions.FieldMessage;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+		User user = repository.findByEmail(dto.getEmail());
+		if (user != null) {
+			list.add(new FieldMessage("email", "Email ja existe no banco de dados"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
