94d298d4b1756939ef45c108fc9ade4d2c8658cb
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-07-07 15:07:36 +0200

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <h1>Hello DSCatalog</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
