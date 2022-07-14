eae51aef72e7d8b2e63b95dc96fffcf025a3f309
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-07-14 13:04:17 +0200

    Catalog listing

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -22,7 +22,7 @@ const ProductDetails = () => {
     axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
       setProduct(response.data);
     });
-  }, []);
+  }, [productId]);
 
   return (
     <div className="product-details-container">
