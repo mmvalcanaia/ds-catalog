1bf1a145c6960c8923e82f3c7c30faab2afc10fd
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-08-02 07:57:25 +0200

    Product list from backend

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,35 +1,32 @@
 import './styles.css';
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Product } from 'types/product';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 const List = () => {
-  const product = {
-    id: 2,
-    name: 'Smart TV',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Livros',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
       },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-      {
-        id: 2,
-        name: 'Eletrônicos',
-      },
-    ],
-  };
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
-    <div className='product-crud-container'>
+    <div className="product-crud-container">
       <div className="product-crud-bar-container">
         <Link to="admin/products/create">
           <button className="btn btn-primary text-white btn-crud-add">
@@ -39,15 +36,11 @@ const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
