5a64674135abe475fc93312d469d39184aac5cef
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-08-04 10:39:38 +0200

    Pagination on Catalog and css correction

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -12,22 +12,22 @@ const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   useEffect(() => {
-    getProducts();
+    getProducts(0);
   }, []);
 
-  const getProducts = () => {
+  const getProducts = (pageNUmber : number) => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: 0,
-        size: 50,
+        page: pageNUmber,
+        size: 3,
       },
     };
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  }
+  };
 
   return (
     <div className="product-crud-container">
@@ -42,11 +42,11 @@ const List = () => {
       <div className="row">
         {page?.content.map((product) => (
           <div key={product.id} className="col-sm-6 col-md-12">
-            <ProductCrudCard product={product} onDelete={() => getProducts()}/>
+            <ProductCrudCard product={product} onDelete={() => getProducts(page.number)} />
           </div>
         ))}
       </div>
-      <Pagination />
+      <Pagination pageCount={page ? page.totalPages : 0} range={3} onChange={getProducts}/>
     </div>
   );
 };
--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -14,18 +14,17 @@ const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
+  const getProducts = (pageNumber: number) => {
     const params: AxiosRequestConfig = {
       method: 'GET',
-      url: "/products",
+      url: '/products',
       params: {
-        page: 0,
+        page: pageNumber,
         size: 12,
       },
     };
 
     setIsLoading(true);
-
     requestBackend(params)
       .then((response) => {
         setPage(response.data);
@@ -33,6 +32,10 @@ const Catalog = () => {
       .finally(() => {
         setIsLoading(false);
       });
+  };
+
+  useEffect(() => {
+    getProducts(0);
   }, []);
 
   return (
@@ -41,19 +44,26 @@ const Catalog = () => {
         <h1>Catálogo de produtos</h1>
       </div>
       <div className="row">
-        {isLoading ? <CardLoader /> :(
+        {isLoading ? (
+          <CardLoader />
+        ) : (
           page?.content.map((product) => {
-          return (
-            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
-              <Link to="/products/1">
-                <ProductCard product={product} />
-              </Link>
-            </div>
-          );
-        }))}
+            return (
+              <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+                <Link to="/products/1">
+                  <ProductCard product={product} />
+                </Link>
+              </div>
+            );
+          })
+        )}
       </div>
       <div className="row">
-        <Pagination />
+        <Pagination
+          pageCount={page ? page.totalPages : 0}
+          range={3}
+          onChange={getProducts}
+        />
       </div>
     </div>
   );
