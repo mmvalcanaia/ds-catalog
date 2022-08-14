9b6f66519d34e611eb577635a18354094787afc5
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-08-14 11:34:20 +0200

    Refactor token, role type

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -6,7 +6,7 @@ import { useContext, useState } from 'react';
 import { AuthContext } from 'AuthContext';
 import { requestBackendLogin } from 'util/requests';
 import { saveAuthData } from 'util/storage';
-import { getTokenData } from 'util/auth';
+import { getTokenData } from 'util/token';
 
 type Credentials = {
   username: string;
--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,12 +1,25 @@
 import { formatPrice } from 'util/formatters';
 
-test('formatPrice should format number pt-BR when value 10.1 given', () => {
-  //ARRANGE
-  const value = 10.1;
-
-  //ACT
-  const result = formatPrice(value);
-
-  //ASSERT
-  expect(result).toEqual('10,10');
+describe('format price with positive numbers', () => {
+  test('formatPrice should format number pt-BR when given value 10.1', () => {
+    const result = formatPrice(10.1);
+    expect(result).toEqual('10,10');
+  });
+  test('formatPrice should format number pt-BR when given value 0.1', () => {
+    const result = formatPrice(0.1);
+    expect(result).toEqual('0,10');
+  });
 });
+
+
+describe('format price with non-positive numbers', () => {
+    test('formatPrice should format number pt-BR when given value 0', () => {
+      const result = formatPrice(0);
+      expect(result).toEqual('0,00');
+    });
+    test('formatPrice should format number pt-BR when given value -5.1', () => {
+      const result = formatPrice(-5.1);
+      expect(result).toEqual('-5,10');
+    });
+  });
+  
