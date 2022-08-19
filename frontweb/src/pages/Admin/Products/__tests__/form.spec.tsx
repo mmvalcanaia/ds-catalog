f5d6dd47d984ec185ee5a6cdcfd6937b5a3666a9
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2022-08-19 16:09:00 +0200

    test: should show toast and redirect when form submited correctly

--- frontweb/src/pages/Admin/Products/__tests__/form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/form.spec.tsx
@@ -5,7 +5,7 @@ import selectEvent from 'react-select-event';
 import { ToastContainer } from 'react-toastify';
 import history from 'util/history';
 import Form from '../Form';
-import { server } from './fixtures';
+import { productResponse, server } from './fixtures';
 import '@testing-library/jest-dom';
 
 beforeAll(() => server.listen());
@@ -105,9 +105,53 @@ describe('Product form create tests', () => {
     userEvent.type(descriptionInput, 'Computador de última geração');
 
     await waitFor(() => {
-        const messages = screen.queryAllByText('Campo obrigatório');
-        expect(messages).toHaveLength(0);
-      });
+      const messages = screen.queryAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(0);
+    });
+  });
+});
+
+describe('Product form update tests', () => {
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+      productId: '1',
+    });
   });
 
+  test('Should show toast and redirect when form submited correctly', async () => {
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    );
+
+    await waitFor(() => {
+      const nameInput = screen.getByTestId('name');
+      const priceInput = screen.getByTestId('price');
+      const imgUrlInput = screen.getByTestId('imgUrl');
+      const descriptionInput = screen.getByTestId('description');
+      const formElement = screen.getByTestId('form');
+
+      expect(nameInput).toHaveValue(productResponse.name);
+      expect(priceInput).toHaveValue(String(productResponse.price));
+      expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+      expect(descriptionInput).toHaveValue(productResponse.description);
+
+      const categoriesIds = productResponse.categories.map(element => String(element.id));
+      expect(formElement).toHaveFormValues({categories: categoriesIds});
+    });
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const toastElement = screen.getByText('Produto cadastrado com sucesso!');
+      expect(toastElement).toBeInTheDocument();
+    });
+
+    expect(history.location.pathname).toEqual('/admin/products');
+
+  });
 });
